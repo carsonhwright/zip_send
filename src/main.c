b977be0540d9fdc05a7882fd4bd7cca227f2db94
Carson Wright <carsonhwright@gmail.com>
2025-06-04 14:36:36 +0000

    newlines are weird with fputs

--- src/main.c
+++ src/main.c
@@ -9,14 +9,18 @@ int main() {
 	FILE* fptr;
 	// open file pointer at WRITE_DIR
 	fptr = fopen(WRITE_DIR, "w");
-	
+	char data[20] = "Fuck a white bitch!";
 	// check that the file isn't jacked up
 	if (fptr == NULL)
 		printf("File is fucked\n");
 	else
 		printf("The file, she is-a good!\n");
-	fclose(fptr);		
-	printf("Fuck.\n");
+
+	// write some bullshit to the file
+	fputs(data, fptr);
+	fputs("\n", fptr);
+	fclose(fptr);
+	printf("Fuck. It's done.\n");
 	return 0;
 
 }
